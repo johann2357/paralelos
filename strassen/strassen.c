4af94cc2672db18a211263fd7666b2d36bf3bfdf
johann2357 <johann2357@gmail.com>
2015-03-31 22:39:08 -0500

    basic operations

--- strassen/strassen.c
+++ strassen/strassen.c
@@ -10,6 +10,7 @@ using namespace std;
 
 #define MAX_NUMBER 100
 
+
 void normalize(vector<vector<int>>* m) {
   double total_row = pow(2.0, ceil(log2((*m)[0].size())));
   double total_column = pow(2.0, ceil(log2((*m).size())));
@@ -17,23 +18,55 @@ void normalize(vector<vector<int>>* m) {
   double to_fill_row = total - (*m)[0].size();
   double to_fill_column = total - (*m).size();
   for (auto &v: (*m))
-    for (int i = 0; i < to_fill_row; i++)
+    for (int i = 0; i < to_fill_row; ++i)
       v.push_back(0);
   vector<int> tmp(total, 0);
-  for (int i = 0; i < to_fill_column; i++)
+  for (int i = 0; i < to_fill_column; ++i)
     (*m).push_back(tmp);
+}
+
+
+void sum(vector<vector<int>>* m1, vector<vector<int>>* m2,
+         vector<vector<int>>* result) {
+  for (int i = 0; i < (*m1).size(); ++i) {
+    for (int j = 0; j < (*m1).size(); ++j) {
+      (*result)[i][j] = (*m1)[i][j] + (*m2)[i][j];
+    }
+  }
+}
+
+void substract(vector<vector<int>>* m1, vector<vector<int>>* m2,
+         vector<vector<int>>* result) {
+  for (int i = 0; i < (*m1).size(); ++i) {
+    for (int j = 0; j < (*m1).size(); ++j) {
+      (*result)[i][j] = (*m1)[i][j] - (*m2)[i][j];
+    }
+  }
+}
+
+
+void multiply(vector<vector<int>>* m1, vector<vector<int>>* m2,
+              vector<vector<int>>* result) {
+  for (int i = 0; i < (*m1).size(); i++) {
+    for (int k = 0; k < (*m1).size(); k++) {
+      for (int j = 0; j < (*m1).size(); j++) {
+        (*result)[i][j] += (*m1)[i][k] * (*m2)[k][j];
+      }
+    }
+  }
+}
+
+void strassen(vector<vector<int>>* m1, vector<vector<int>>* m2,
+              vector<vector<int>>* result) {
+
 }
 
 void print_matrix(vector<vector<int>>* m) {
   for (auto &vv: (*m)) {
     for (auto &v: vv)
-      cout << v << "\t\t";
+      cout << v << "\t";
     cout << endl;
   }
-}
-
-void strassen(vector<vector<int>>* m1, vector<vector<int>>* m2) {
-
 }
 
 void generate_random(vector<vector<int>>* m, int rows, int cols) {
