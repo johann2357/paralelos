702a54cec5300f1f40277582c0103ded1dd1b569
johann2357 <johann2357@gmail.com>
2015-03-31 21:53:35 -0500

    small fixes and better organization

--- strassen/strassen.c
+++ strassen/strassen.c
@@ -2,45 +2,55 @@
 #include <vector>
 #include <algorithm>
 #include <cstdlib>
-#include <math.h> 
+#include <math.h>
 #include <stdlib.h>
 
 using namespace std;
 
+
+#define MAX_NUMBER 100
+
 void normalize(vector<vector<int>>* m) {
-  double to_fill_row = ceil(log2((*m)[0].size()));
-  double to_fill_column = ceil(log2((*m).size()));
-  double total_row = to_fill_row + (*m)[0].size();
-  double total_column = to_fill_column + (*m).size();
-  double total = max(to_fill_row, to_fill_column);
-  double amount = abs(total - (*m)[0].size());
-  for (auto v: (*m)) {
-    for (int i = 0; i < amount; i++)
+  double total_row = pow(2.0, ceil(log2((*m)[0].size())));
+  double total_column = pow(2.0, ceil(log2((*m).size())));
+  double total = max(total_row, total_column);
+  double to_fill_row = total - (*m)[0].size();
+  double to_fill_column = total - (*m).size();
+  for (auto &v: (*m))
+    for (int i = 0; i < to_fill_row; i++)
       v.push_back(0);
-  }
-  amount = abs(total - (*m).size());
   vector<int> tmp(total, 0);
-  for (int i = 0; i < amount; i++)
+  for (int i = 0; i < to_fill_column; i++)
     (*m).push_back(tmp);
+}
+
+void print_matrix(vector<vector<int>>* m) {
+  for (auto &vv: (*m)) {
+    for (auto &v: vv)
+      cout << v << "\t\t";
+    cout << endl;
+  }
 }
 
 void strassen(vector<vector<int>>* m1, vector<vector<int>>* m2) {
 
 }
 
+void generate_random(vector<vector<int>>* m, int rows, int cols) {
+  for (int i = 0; i < rows; ++i) {
+    vector<int> tmp(cols);
+    generate(tmp.begin(), tmp.end(), rand);
+    for (auto &n: tmp)
+      n = n % MAX_NUMBER;
+    (*m).push_back(tmp);
+  }
+}
+
 int main() {
-  vector<int> myVector(10);
-  generate(myVector.begin(), myVector.end(), rand);
-  for (auto v: myVector) {
-    cout << v << " ";
-  }
-  cout << endl;
   vector<vector<int>> test;
-  test.push_back(myVector);
+  generate_random(&test, 3, 5);
+  print_matrix(&test);
+  cout << endl;
   normalize(&test);
-  for (auto vv: test) {
-    for (auto v: vv)
-      cout << v << " ";
-    cout << endl;
-  }
+  print_matrix(&test);
 }
